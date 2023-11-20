ed1ef4a5a61f7534e76f973ca7e35e05a3d6353c
Ubuntu <vid_embedding_usr@video-embedding-new.sftob3mab2aeritnwpaizpdqeb.bx.internal.cloudapp.net>
2023-11-20 17:08:34 +0000

    Cont

--- summarize_stats.py
+++ summarize_stats.py
@@ -10,20 +10,20 @@ stats_csv_name =  "motion_per_frame.csv"
 # make target list
 def summarize_stats(root_dir):
    target_list = []
-   for dirpath, dirs, files in os.walk(root_dir):
-         for f in files:
-             print(f"{f}")
-             if f == stats_csv_name :
-                stats_path = os.path.join(dirpath, f)
-                df2=pd.read_csv(stats_path)
-                print(df2.keys())
-                #frame_number,motion,num_faces,num_person
-                num_faces=df2['num_faces'].sum()
-                num_person=df2['num_person'].sum()
-                avg_motion=df2['motion'].mean()
-                with open('summary_stats.csv', 'a+') as f:
-                    w = csv.writer(f, delimiter=' ')
-                    w.writerow( f"{stats_path},{avg_motion},{num_faces},{num_person}" )
+   with open('summary_stats.csv', 'w') as f:
+       w = csv.writer(f,delimiter=',')
+       for dirpath, dirs, files in os.walk(root_dir):
+           for f in files:
+               print(f"{f}")
+               if f == stats_csv_name :
+                   stats_path = os.path.join(dirpath, f)
+                   df2=pd.read_csv(stats_path)
+                   print(df2.keys())
+                #frame_number,motion,num_faces,num_person 
+                   num_faces=df2['num_faces'].sum()
+                   num_person=df2['num_person'].sum()
+                   avg_motion=df2['motion'].mean()
+                   w.writerow( [stats_path,avg_motion,num_faces,num_person] )
 
    return 
 
