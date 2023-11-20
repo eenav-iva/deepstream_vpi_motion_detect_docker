7306bf001b5f56032f1029d7f3121d385b604474
Ubuntu <vid_embedding_usr@video-embedding-new.sftob3mab2aeritnwpaizpdqeb.bx.internal.cloudapp.net>
2023-11-20 16:34:35 +0000

    Add summarize stats script to find top videoss

---
+++ summarize_stats.py
@@ -0,0 +1,43 @@
+import os
+import pandas as pd
+import sys 
+import csv
+home = os.path.expanduser("~")
+#root_dir = os.path.join(home, "/opt/data/")
+stats_csv_name =  "motion_per_frame.csv"
+
+
+# make target list
+def summarize_stats(root_dir):
+   target_list = []
+   for dirpath, dirs, files in os.walk(root_dir):
+         for f in files:
+             print(f"{f}")
+             if f == stats_csv_name :
+                stats_path = os.path.join(dirpath, f)
+                df2=pd.read_csv(stats_path)
+                print(df2.keys())
+                #frame_number,motion,num_faces,num_person
+                num_faces=df2['num_faces'].sum()
+                num_person=df2['num_person'].sum()
+                avg_motion=df2['motion'].mean()
+                with open('summary_stats.csv', 'a+') as f:
+                    w = csv.writer(f, delimiter=' ')
+                    w.writerow( f"{stats_path},{avg_motion},{num_faces},{num_person}" )
+
+   return 
+
+
+def main(args):
+
+# Check input arguments
+    if len(args) < 2:
+        sys.stderr.write("usage: %s path\n" % args[0])
+        sys.exit(1)
+    else:
+        summarize_stats(args[-1])
+    
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))    
+##if __name__ == '__main__':
+#   write_csv()
